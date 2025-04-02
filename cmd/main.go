87c5bb610e72a98f7bac1d409599106c394247fc
Leo-OShea <leo.oshea@wpengine.com>
2025-04-02 16:52:01 +0100

    Implementing API Endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,63 @@ import (
 	"net/http"
 
 	"leo.com/m/internal/handlers"
+	"leo.com/m/internal/models"
 )
 
-func main() {
-	http.HandleFunc("/", handlers.HelloHandler)
+var tasks = []models.Task{
+	{ID: 1, Title: "Task 1", Completed: false},
+	{ID: 2, Title: "Task 2", Completed: false},
+}
 
-	fmt.Println("Server is listening on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+func main() {
+	fmt.Println("Todo List")
+
+	mux := http.NewServeMux()
+
+	// Say hello!!
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.HelloHandler(w, r)
+	})
+
+	// Return all tasks
+	mux.HandleFunc("GET /tasks", func(w http.ResponseWriter, r *http.Request) {
+		handlers.GetTasksHandler(w, r)
+	})
+
+	// Return specific task
+	mux.HandleFunc("GET /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.GetSpecificTaskHandler(w, r)
+	})
+
+	// Add a task
+	mux.HandleFunc("POST /addTask/{title}", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.AddTaskHandler(w, r)
+		title := r.PathValue("title")
+		newTask := models.Task{ID: len(tasks) + 1, Title: title, Completed: false}
+		tasks = append(tasks, newTask)
+		fmt.Fprintf(w, "Added task: %s", title)
+	})
+
+	// Update a task as completed/uncompleted
+	mux.HandleFunc("PUT /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		for i, task := range tasks {
+			if fmt.Sprintf("%d", task.ID) == id {
+				tasks[i].Completed = true
+				fmt.Fprintf(w, "Updated task with ID: %s", id)
+				return
+			}
+		}
+		http.NotFound(w, r)
+	})
+
+	// Delete a task
+	mux.HandleFunc("DELETE /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "Deleted task with ID: %s", id)
+	})
+
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 	}
 }
--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,10 +1,26 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello, World!"))
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Get Tasks")
+
+}
+
+func GetSpecificTaskHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Fprintf(w, "Return task with ID: %s", id)
+}
+
+func AddTaskHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Add Task"))
 }
